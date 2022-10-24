541249ee192b2a8232e37586bc0c24192f85a219
GYN1979 <galeeva.y@yandex.ru>
2022-10-24 20:35:18 +0400

    выполненное дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+// Task1
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(" ФИО сотрудника - " + fullName);
+//Task2
+        String fullName1 = fullName.toUpperCase();
+        System.out.println(" Данные ФИО сотрудника - для заполнения отчета " + fullName1);
+//Task3
+        String fullName2 = " Иванов Семён Семёныч ";
+        String fullName3 = " Иванов Семён Семёныч ".replace("ё", "е");
+        System.out.println(" Данные ФИО сотрудника - " + fullName3);
+}
 }
